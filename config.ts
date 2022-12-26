a1907cabd0ab4314dd403a960d791a233e5e7ec7
DarrenReal <87570990+DarrenReal@users.noreply.github.com>
2022-12-26 19:24:10 +0700

    Update and rename config.example.ts to config.ts

---
+++ config.ts
@@ -0,0 +1,27 @@
+const config: Config = {
+    mode: 'CHINA', // 'CHINA' | 'WORLD'
+    botToken: '5710359563:AAHUBKS4MITztZEnkvQwjd6DSUlc3yUPa0w',
+    keepFiles: {
+        compared: true,
+        input: true,
+        single: true,
+        video: false,
+    },
+    messages: {
+        blocked: 'The Chinese server has blocked the bot, too bad',
+        bye:
+            'ᴛʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ᴜꜱɪɴɢ ᴛʜɪꜱ ʙᴏᴛ 🥰\n' +
+            '@MikaEirene ᴊᴏɪɴ ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ',
+        hello: 'Hi 👋 Send me a photo to convert it into a 2D anime art',
+        media: '@MikaEirene\\_bot',
+        received: 'Photo has been received, please wait',
+    },
+    sendMedia: {
+        compared: true,
+        single: true, // works only with mode=CHINA
+        video: false, // works only with mode=CHINA
+    },
+    proxyUrl: 'socks5://@58.49.230.248:30001',
+};
+
+export default config;
